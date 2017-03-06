4b57cb9ce681134995576ec3da7ad86a4c2fdc3b
Luis Busta <lfbusta@gmail.com>
2017-03-06 16:42:00 +0100

    React Footer
    • Rewrote the footer in react.

--- src/js/footer.js
+++ src/js/footer.js
@@ -1,7 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+class Footer extends React.Component{
+    render(){
+        return(
+            <div className="footerContainer">
+                <a href="mailto:contact@lfbusta.com" id="footer">contact@lfbusta.com</a>
+                <div className="contactIcons">
+                    <a href="https://www.linkedin.com/in/luis-busta-42b621121/" target="_blank"><img src="resources/logo_linkedin2.svg" alt="Linkedin Logo"/></a>
+                    <a href="https://github.com/lfbusta" target="_blank"><img src="resources/logo_github2.svg" alt="Github Logo"/></a>
+                    <a href="https://www.instagram.com/lfbusta" target="_blank"><img src="resources/logo_instagram2.svg" alt="Instagram Logo"/></a>
+                    <a href="https://twitter.com/lfbusta" target="_blank"><img src="resources/logo_twitter2.svg" alt="Twitter Logo"/></a>
+                </div>
+            </div>
+        );
+    }
+}
+
 ReactDOM.render(
-  <h1>Hello, world!</h1>,
+  <Footer/>,
   document.getElementsByTagName('footer')[0]
 );
