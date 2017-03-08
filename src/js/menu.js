d716f67219b70b3f8d7280066d1f76c8106e54c2
Luis Busta <lfbusta@gmail.com>
2017-03-08 15:15:08 +0100

    General HTML Structure
    • Modified the HTML structure to accommodate for the new react layout.

--- src/js/menu.js
+++ src/js/menu.js
@@ -4,13 +4,13 @@ import ReactDOM from 'react-dom';
 class Menu extends React.Component{
     render(){
         return(
-            <div className="containerMenu">
+            <nav className="containerMenu">
                 <a href="index.html" className="buttonMenu buttonMenu_main animation__show_buttonMenu_projects" id="buttonMenu_projects"><img src="../resources/buttonProjects.svg" alt=""/></a>
                 <a href="blog" className="faded buttonMenu buttonMenu_main animation__show_buttonMenu_blog" id="buttonMenu_blog"><img src="../resources/buttonBlog.svg" alt=""/></a>
                 <a href="about" className="faded buttonMenu buttonMenu_main animation__show_buttonMenu_about" id="buttonMenu_about"><img src="../resources/buttonAbout.svg" alt=""/></a>
                 <a href="connect" className="buttonMenu buttonMenu_main animation__show_buttonMenu_connect" id="buttonMenu_connect"><img src="../resources/buttonConnect.svg" alt=""/></a>
                 <img src="../resources/buttonStack.svg" alt="" className="buttonMenu animation__hide_buttonMenu_stack" id="buttonMenu_stack"/>
-            </div>
+            </nav>
         );
     }
 }
