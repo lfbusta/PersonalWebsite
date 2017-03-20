2a71fdd2ba4612bf33d7859218cd21630785f33c
Luis Busta <lfbusta@gmail.com>
2017-03-20 14:43:18 +0100

    Reorganization
    • Reorganized folders to reflext new react layout.

--- src/js/content.js
+++ src/js/content.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom';
 const data = [
     {
         title: "Shardcraft",
-        screenshot: "resources/ss05.png",
+        screenshot: "../resources/ss05.png",
         link: "https://shard.cl",
         aboutShort: "Shardcraft is a small Minecraft server that I’ve been hosting with a friend for almost 5 years now. This interactive page will include a login for admins and players to interact directly with in-game content.",
         aboutLong: "Shardcraft is a small private whitelisted vanilla Minecraft server that I’ve been hosting with a friend for almost 5 years now. It started as my personal single player world but has now grown large enough that it would benefit from its own webpage. This interactive page will include a login for admins and players to get information and interact directly with in-game content through javascript and python scripts.",
@@ -20,7 +20,7 @@ const data = [
     },
     {
         title: "This Website",
-        screenshot: "resources/ss04.png",
+        screenshot: "../resources/ss04.png",
         link: "#",
         aboutShort: "As a webdev it is important to have a place to show your projects and allow people to contact you. This is it. The page you’re on right now.",
         aboutLong: "As a webdev it is important to have a place to show your projects and for people to contact you. This is it. The page you’re on right now. This site will act as a hub for all my projects and, in the near future, will include more information about me (possibly even a blog). The radial menu and all the assets for it were custom built on vanilla javascript.",
@@ -37,7 +37,7 @@ const data = [
     },
     {
         title: "Infestation Sim",
-        screenshot: "resources/ss03.png",
+        screenshot: "../resources/ss03.png",
         link: "https://lfbusta.github.io/BerlinMiniGamejam-Feb2017/",
         aboutShort: "Infestation Sim is a game developed in conjunction with this webpage as part of the Berlin Mini Game Jam of February 2017. It was quickly put together from scratch in less than 8 hours.",
         aboutLong: "Infestation Sim is a short game developed in conjunction with this page as part of the Berlin Mini Game Jam of February 2017. It was put together from scratch in less than 8 hours and it served as my first experience with gulp as a way to automate the build and optimization process for the site.",
@@ -51,7 +51,7 @@ const data = [
     },
     {
         title: "Astromundus Fight",
-        screenshot: "resources/ss02.png",
+        screenshot: "../resources/ss02.png",
         link: "https://cmclaudet.github.io/AstromundusFight/",
         aboutShort: "This webpage was developed to showcase a small game called Astromundus Fight. It was my first multi-column responsive website as well as my introduction to flexbox. It also uses more advanced javascript than Cloud10.",
         aboutLong: "Astromundus Fight is a game developed by Catherine Claudet on LUA and we thought that it probably needed a webpage too. It was my first multi-column responsive website as well as my introduction to flexbox. It also uses more advanced javascript than Cloud10.",
@@ -63,7 +63,7 @@ const data = [
     },
     {
         title: "Cloud10",
-        screenshot: "resources/ss01.png",
+        screenshot: "../resources/ss01.png",
         link: "https://cmclaudet.github.io/Cloud10/",
         aboutShort: "Cloud10 is a small game developed for a Game Jam. I made a site for it as part of my learning process. This is the first complete webpage I ever made.",
         aboutLong: "Cloud10 is a small prototype for a game developed by Catherine Claudet for a Game Jam. I needed some inspiration to put to use all that I had learned until then. Since the assets were already made, Cloud10 was the perfect experiment. This is a very barebones single-page responsive website with only some minor use of javascript.",
@@ -82,7 +82,7 @@ class Project extends React.Component{
         let logo = "";
         for(let i=0; i<this.props.data.technologies.length; i++){
             tech = this.props.data.technologies[i];
-            logo = `resources/logo_${tech.toLowerCase()}.png`;
+            logo = `../resources/logo_${tech.toLowerCase()}.png`;
             techLogoList.push(
                 <div className="tech" key={tech.toLowerCase()}>
                     <img className="techLogo" src={logo} alt={`${tech} Logo`} />
--- src/js/menu.js
+++ src/js/menu.js
@@ -5,11 +5,11 @@ class Menu extends React.Component{
     render(){
         return(
             <nav className="containerMenu">
-                <a href="index.html" className="buttonMenu buttonMenu_main animation__show_buttonMenu_projects" id="buttonMenu_projects"><img src="resources/buttonProjects.svg" alt=""/></a>
-                <a href="blog" className="faded buttonMenu buttonMenu_main animation__show_buttonMenu_blog" id="buttonMenu_blog"><img src="resources/buttonBlog.svg" alt=""/></a>
-                <a href="about" className="faded buttonMenu buttonMenu_main animation__show_buttonMenu_about" id="buttonMenu_about"><img src="resources/buttonAbout.svg" alt=""/></a>
-                <a href="connect" className="buttonMenu buttonMenu_main animation__show_buttonMenu_connect" id="buttonMenu_connect"><img src="resources/buttonConnect.svg" alt=""/></a>
-                <img src="resources/buttonStack.svg" alt="" className="buttonMenu buttonMenu_main animation__hide_buttonMenu_stack" id="buttonMenu_stack"/>
+                <a href="../public" className="buttonMenu buttonMenu_main animation__show_buttonMenu_projects" id="buttonMenu_projects"><img src="../resources/buttonProjects.svg" alt=""/></a>
+                <a href="../blog" className="faded buttonMenu buttonMenu_main animation__show_buttonMenu_blog" id="buttonMenu_blog"><img src="../resources/buttonBlog.svg" alt=""/></a>
+                <a href="../about" className="faded buttonMenu buttonMenu_main animation__show_buttonMenu_about" id="buttonMenu_about"><img src="../resources/buttonAbout.svg" alt=""/></a>
+                <a href="../connect" className="buttonMenu buttonMenu_main animation__show_buttonMenu_connect" id="buttonMenu_connect"><img src="../resources/buttonConnect.svg" alt=""/></a>
+                <img src="../resources/buttonStack.svg" alt="" className="buttonMenu buttonMenu_main animation__hide_buttonMenu_stack" id="buttonMenu_stack"/>
             </nav>
         );
     }
